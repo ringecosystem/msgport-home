5791d9ecb335dba04bc74e67b2684bafbe577427
Hamid Roohi <hamidroohi71@gmail.com>
2024-12-19 11:22:27 +0330

    fix(deploy error): fix a type error in deployment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,10 @@
 import { codeBlocks } from "../data/code";
-import useWidth from "../hooks/useWidth";
+// import useWidth from "../hooks/useWidth";
 import Container from "./Container";
 import PrettyCode from "./PrettyCode";
 
 const Hero = () => {
-  const width = useWidth();
+  // const width = useWidth();
   return (
     <Container classes="bg-cover lg:relative pt-[7.5rem] pb-[6.25rem] lg:mt-0">
       <h1 className="text-center text-white font-normal text-[4.125rem] leading-[5.3rem]">
