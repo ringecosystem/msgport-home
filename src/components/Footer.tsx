42382f822891ce443d2d9dd952b05649844700d4
Hamid Roohi <hamidroohi71@gmail.com>
2024-12-30 13:32:01 +0330

    fix(mobile menu): fix mobile menu style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,19 +3,21 @@ import data from "../data/socials.json";
 export default function Footer() {
   return (
     <section
-      className="flex justify-between items-center py-[2.375rem] px-[6.25rem]"
+      className="flex flex-col-reverse lg:flex-col gap-[20px] justify-between items-center py-[23px] lg:py-[2.375rem] lg:px-[6.25rem]"
       style={{
         backgroundImage: "linear-gradient(to top,#00D4484D, #00D44800",
       }}
     >
-      <p className="text-[1rem] text-white font-[300]">@ 2024 Msgport</p>
+      <p className="text-[12px] lg:text-[1rem] text-white font-[300]">
+        @ 2024 Msgport
+      </p>
       <div className="flex items-center gap-[1.875rem]">
         {data.socials.map((item: any) => (
           <a href={item.href} key={item.id} target="_blank">
             <img
               src={item.icon}
               alt={item.name}
-              className="w-[1.875rem] h-[1.875rem] object-contain"
+              className="w-[24px] h-[24px] lg:w-[1.875rem] lg:h-[1.875rem] object-contain"
             />
           </a>
         ))}
